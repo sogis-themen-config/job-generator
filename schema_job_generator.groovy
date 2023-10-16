8a87ac0dea1b47f3582b13bc3ca34b0298174c62
Stefan Ziegler <edi.gonzales@gmail.com>
2023-10-16 19:56:34 +0200

    Update schema_job_generator.groovy

--- schema_job_generator.groovy
+++ schema_job_generator.groovy
@@ -3,6 +3,7 @@ def jobGeneratorRepoName = 'job-generator'
 
 // Weil es mehrere Jobs geben kann innerhalb eines Repo, reicht das Wissen, dass es ein Repo gibt, nicht aus.
 // Eher eine Datei (testweise Array im Code) mit allen Repos und Schema-Jobs.
+// Die Datei könnte später ein Pipeline nachführen, wenn einen neuen Schema-Job erstellt (im Themen-Repo).
 
 repoApi = new URL("https://api.github.com/orgs/${organization}/repos")
 repos = new groovy.json.JsonSlurper().parse(repoApi.newReader())
